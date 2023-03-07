e4fcec599f4c885a6cbd60b8267d41f9caf3d560
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2023-03-07 14:48:13 +0100

    feat: install and start using puppeteer

--- audit/packages/cli/src/main.ts
+++ audit/packages/cli/src/main.ts
@@ -1,3 +1,40 @@
-import {exporterLogger} from '@audit/exporter-logger'
-exporterLogger('Audit')
-process.exit(0)
+import { exporterLogger } from '@audit/exporter-logger';
+import puppeteer, { Page } from 'puppeteer';
+
+const getHomePageTitle = (page: Page): Promise<string> => {
+    return page.title();
+};
+
+const getLang = (page: Page): Promise<string> => {
+    return page.evaluate(() => document.querySelector('html').getAttribute("lang"));
+};
+
+interface PageAuditResult {
+    title?: string;
+    lang?: string;
+}
+
+type AuditResults = { [url: string]: PageAuditResult};
+
+const auditExternalWebPage = async (url: string, page: Page): Promise<PageAuditResult> => {
+    const pageAuditResult: PageAuditResult = {};
+    await page.goto('https://www.emmanueldemey.dev/'); 
+    pageAuditResult.title = await getHomePageTitle(page);
+    pageAuditResult.lang = await getLang(page);
+    return pageAuditResult;
+}
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  const results: AuditResults = {} 
+  
+  const url = 'https://www.emmanueldemey.dev/';
+  results[url] = await auditExternalWebPage(url, page);
+
+  await browser.close();
+
+  exporterLogger(JSON.stringify(results));
+  process.exit(0);
+})();
