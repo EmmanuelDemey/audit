c3d828f47482609a8849ebe141bd0bf07a6c1a7d
Emmanuel <demey.emmanuel@gmail.com>
2024-01-22 08:43:30 +0100

    feat: add CLI NPM module

--- audit/apps/audit/src/main.ts
+++ audit/apps/audit/src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+const { program } = require('commander');
+
+program.name('audit').version('0.0.0');
+
+program
+  .command('audit')
+  .argument('<string>', 'url to audit')
+  .option('--path <char>', 'path to a folder container the project')
+  .action((url, options) => {
+    console.log(options, url);
+  });
+
+program.parse();
