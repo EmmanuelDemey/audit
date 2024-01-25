ec9ae6706694d09a1d6080fedb9fad75c802db87
Emmanuel <demey.emmanuel@gmail.com>
2024-01-25 22:58:53 +0100

    feat: check if the URL is wellformed

--- audit/apps/audit/src/main.ts
+++ audit/apps/audit/src/main.ts
@@ -1,4 +1,5 @@
 const { program } = require('commander');
+const { exists } = require('fs');
 
 program.name('audit').version('0.0.0');
 
@@ -6,8 +7,13 @@ program
   .command('audit')
   .argument('<string>', 'url to audit')
   .option('--path <char>', 'path to a folder container the project')
-  .action((url, options) => {
-    console.log(options, url);
+  .action(async (urlString, options) => {
+    try {
+      const url = new URL(urlString);
+    } catch (e) {
+      console.error(`The url ${urlString} is not a valid URL`);
+      process.exit(1);
+    }
   });
 
 program.parse();
