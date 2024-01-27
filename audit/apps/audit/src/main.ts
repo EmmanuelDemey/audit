fce0931299c502389b298b1ff6ed939cd783799a
Emmanuel <demey.emmanuel@gmail.com>
2024-01-27 20:04:56 +0100

    feat: url can be an array

--- audit/apps/audit/src/main.ts
+++ audit/apps/audit/src/main.ts
@@ -7,11 +7,11 @@ program.name('audit').version('0.0.0');
 
 program
   .command('audit')
-  .argument('<string>', 'url to audit')
+  .argument('<string...>', 'url to audit')
   .option('--path <char>', 'path to a folder container the project')
   .action(async (urlString, options) => {
     try {
-      const url = new URL(urlString);
+      //const url = new URL(urlString);
     } catch (e) {
       console.error(`The url ${urlString} is not a valid URL`);
       process.exit(1);
